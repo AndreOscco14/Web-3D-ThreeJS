52eb249997283d67075a46613eb03081ffd4e404
bigDre82 <andre.oscco.14@gmail.com>
2023-04-11 00:59:35 +0200

    Prueba web 3D terminada - BETA

--- main.js
+++ main.js
@@ -82,6 +82,25 @@ const moon = new THREE.Mesh(
 );
 
 scene.add(moon)
+moon.position.z = 30;
+moon.position.setX(-10)
+
+
+function moveCamera(){
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05
+  moon.rotation.y += 0.075
+  moon.rotation.z += 0.05
+  
+  dre.rotation.y += 0.01;
+  dre.rotation.z += 0.01
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera
+moveCamera()
 
 // ------------- Animacion del aro 
 function animate(){
