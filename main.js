d99ec8851fb29dbbeeac755473d79db273872a83
bigDre82 <andre.oscco.14@gmail.com>
2023-04-10 23:52:51 +0200

    Figura estrella

--- main.js
+++ main.js
@@ -3,24 +3,29 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
+
+
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight)
 camera.position.setZ(30);
 
 renderer.render(scene,camera)
-
+//  FIGURA TORUS
 const geometry = new THREE.TorusGeometry(10, 3,16,100)
 // const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
 const material = new THREE.MeshStandardMaterial({color: 0xFF6347 })
 const torus = new THREE.Mesh(geometry, material)
 
 scene.add(torus)
+
 //* ======================== LUCES ===============================
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5,5,5)
@@ -31,10 +36,19 @@ scene.add(pointLight, ambientLight)
 
 // Ayuda en la pantalla
 const lighHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200,50);
+const gridHelper = new THREE.GridHelper(200,80);
 scene.add(lighHelper,  gridHelper)
-
 //* ======================== FIN LUCES ===============================
+
+// Controles para mover la orbitra y figura en imagen
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Figura Star
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+}
 
 
 function animate(){
@@ -42,6 +56,9 @@ function animate(){
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.1
+
+  controls.update();
+
   renderer.render(scene,camera)
 }
 
